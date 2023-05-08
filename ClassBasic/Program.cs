442152b3f2b02c8a32cf958344430c05fac3d6ac
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-05-08 12:17:06 +0900

    ✨ : 클래스 이름 같을 때 충돌 예시

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ClassBasic
 {
-    class Program
+    internal class Program
     {
+        private class Math {
+            /*static public int Abs(int a)
+            {
+                return 0;
+            }*/
+        }
+
         static void Main(string[] args)
         {
+            Math math = new Math();
+            //Console.WriteLine(Math.Abs(-10));
+
             ThirdClass abc;
             ABCClass abc2;
             InnderClass inn;
