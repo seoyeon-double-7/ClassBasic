975f6a161f9c4b579a8752e14c61ecc12f164a76
Seoyeon Bae <s2103@e-mirim.hs.kr>
2023-05-08 12:12:33 +0900

    초기 커밋.

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThirdClass abc;
+            ABCClass abc2;
+            InnderClass inn;
+            Console.WriteLine("Hello World!");
+        }
+
+        private class InnderClass
+        {
+        }
+    }
+
+    internal class ABCClass
+    {
+    }
+}
